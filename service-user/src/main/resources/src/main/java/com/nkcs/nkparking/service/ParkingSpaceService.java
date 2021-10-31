00078b4f60adb0da517fc52beb4635c7b3575cd0
TumbledoreAlalalala <1065571228@qq.com>
2021-11-01 00:07:10 +0800

    Revert "Add files via upload"
    This reverts commit 87da175b33045f468a2ad6ff33af9da6012a2877.

--- service-user/src/main/resources/src/main/java/com/nkcs/nkparking/service/ParkingSpaceService.java
+++
@@ -1,9 +0,0 @@
-package com.nkcs.nkparking.service;
-
-import java.util.List;
-
-public interface ParkingSpaceService {
-    Object querySpaceById(Integer spaceId);
-
-    List<Integer> querySpaceByTime(Long startTime, Long endTime);
-}
