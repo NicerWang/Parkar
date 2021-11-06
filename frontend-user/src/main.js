ab63ce56aea99f717bdf69a0b7869cd3c4379132
NicerWang <nicer_wang@qq.com>
2021-11-06 23:59:01 +0800

    bug fix

--- frontend-user/src/main.js
+++ frontend-user/src/main.js
@@ -6,7 +6,6 @@ import axios from "axios";
 import "bootstrap";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import "animate.css"
-import "echarts-wordcloud"
 
 axios.defaults.baseURL = "/api/";
 // axios.defaults.baseURL = "http://81.70.254.128/api/"
