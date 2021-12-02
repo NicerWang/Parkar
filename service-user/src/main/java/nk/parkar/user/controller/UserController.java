dae8f929468179143f8f21b99c41f2fd485738dc
Nicer_Wang <nicer_wang@qq.com>
2021-12-02 11:48:59 +0800

    Revert.

--- service-user/src/main/java/nk/parkar/user/controller/UserController.java
+++ service-user/src/main/java/nk/parkar/user/controller/UserController.java
@@ -56,20 +56,20 @@ public class UserController {
         return R.error().message("Update Failed");
     }
 
-    @GetMapping("/userExist/{userId}")
+    @GetMapping("/userExit/{userId}")
     public R isUserExit(@PathVariable String userId){
         boolean userExit = userService.isUserExit(userId);
         if(userExit) {
-            return R.ok().message("User exist");
+            return R.ok().message("User exit");
         }
-        return R.error().message("User not exist");
+        return R.error().message("User not exit");
     }
 
     @GetMapping("/getAllUsersInformation")
     public R getAllUsersInformation(HttpServletRequest request){
         boolean isAdmin = JWTUtil.checkAdmin(request.getHeader("token"));
         if(!isAdmin){
-            return R.error().message("You are not an admin!");
+            return R.error().message("You are not a admin!");
         }
         List<User> allUsers = userService.getAllUsersInformation();
         return R.ok().message("Query successful!").data("allUsers",allUsers);
